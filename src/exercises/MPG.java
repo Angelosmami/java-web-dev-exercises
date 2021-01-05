66b83b1aca6d300ee9599f15eea3bd5cadb20879
Jennifer <Angelosmami@yahoo.com>
2021-01-04 21:24:25 -0800

    numeric types exercise

---
+++ src/exercises/MPG.java
@@ -0,0 +1,14 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class MPG {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many miles have you driven?");
+        double miles = input.nextDouble();
+        System.out.println("How many gallons of gas did you consume?");
+        double gas = input.nextDouble();
+        System.out.println((miles/gas)+ "MPG");
+    }
+}
