2b276b23d005f349c8181e34ea820238e204b468
Jennifer <Angelosmami@yahoo.com>
2021-01-17 20:35:41 -0800

    finished exercise4

--- src/org/launchcode/java/demos/lsn3classes1/Course.java
+++ src/org/launchcode/java/demos/lsn3classes1/Course.java
@@ -1,14 +1,27 @@
 package org.launchcode.java.demos.lsn3classes1;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Objects;
 
 public class Course {
     //Student student = new Student("Jen",1,4.0);
 
     private String name;
-    private int numOfCredits;
+    private int numberOfCredits;
     private ArrayList<Student> students;
 
+    public String toString(){
+        return name + " Number Of Credits " + numberOfCredits;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, numberOfCredits);
+    }
+
+    public boolean equals (Object Student){
+     return true;
+    }
 
 
 }
